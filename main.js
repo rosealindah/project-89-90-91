a0b17859d569dfdb653ea04c8102b0dcd368d56d
rosealindah <76764225+rosealindah@users.noreply.github.com>
2021-04-28 20:46:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function adduser(){
+    player1_name=document.getElementById("p1_input").value ;
+    player2_name=document.getElementById("p2_input").value ;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);  
+    window.location="quiz_page.html";
+}
